beeee5b09c14b7a57fa0abf9cd127bfda2ba3cc5
Isadora Palhoti <isadorapalhoti@gmail.com>
2022-12-01 18:37:57 -0300

    Estrutura do service criada

--- src/main/java/com/isadora/employeemanager/service/EmploeeyService.java
+++ src/main/java/com/isadora/employeemanager/service/EmploeeyService.java
@@ -1,10 +1,12 @@
 package com.isadora.employeemanager.service;
 
+import com.isadora.employeemanager.exception.UserNotFoundException;
 import com.isadora.employeemanager.model.Employee;
 import com.isadora.employeemanager.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -19,5 +21,22 @@ public class EmploeeyService {
     public Employee addEmployee(Employee employee){
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return employeeRepo.save(employee);
+    }
+
+    public List<Employee> finAllEmployees(){
+    return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id){
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("User by ID " + id + " not found"));
+    }
+
+    public void deleteEmployee(Long id){
+        employeeRepo.deleteEmployeeById(id);
     }
 }
